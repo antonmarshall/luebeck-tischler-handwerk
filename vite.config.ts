d782c1a8e55fe8df3b130b46dd604c8481f2531a
antonmarshall <anton.marshall03@gmail.com>
2025-04-22 10:45:37 +0200

    check paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: process.env.BASE_URL || '/luebeck-tischler-handwerk/',
   server: {
     host: "::",
     port: 8080,
